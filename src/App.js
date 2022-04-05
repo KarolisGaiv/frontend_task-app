f0efa4a7e3120ceea63ce281284738973980c13f
KarolisGaiv <kgaivenis@gmail.com>
2022-04-05 18:49:11 +0300

    Add content to header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./styles/main.scss"
+import "./styles/main.scss";
+import MoonIcon from "./images/icon-moon.svg";
 
 function App() {
   return (
     <main className="content">
-      <header className="content__header"></header>
+      <header className="content__header">
+        todo
+        <img src={MoonIcon} alt="moon icon" className="content__header__icon" />
+      </header>
     </main>
   );
 }
