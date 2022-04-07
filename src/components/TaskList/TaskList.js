b2faf3b29fccffe843c60afece3fd114e4f32b25
KarolisGaiv <kgaivenis@gmail.com>
2022-04-07 19:02:57 +0300

    Add basic structure for task container

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -5,7 +5,13 @@ function TaskList({ tasks }) {
   return (
     <div className="tasks-wrapper">
       {tasks.map((task) => {
-        return <div>{task.name}</div>;
+        return (
+          <div className="task-container">
+            <button className="task-container__complete-btn"></button>
+            <p className="task-container__task">{task.name}</p>
+            <button className="task-container__delete-btn"></button>
+          </div>
+        );
       })}
     </div>
   );
